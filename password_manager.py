1e0831730322e038c418e579c5eb5cbbf8f8e449
Clinton Osawe <osaweclinton1@gmail.com>
2025-09-20 22:44:46 -0400

    added login function

--- password_manager.py
+++ password_manager.py
@@ -9,3 +9,20 @@ def create_account():
     hashed_password = hashlib.sha256(password.encode()).hexdigest() 
     password_manager[username] = hashed_password 
     print("Account created successfully!")
+
+def create_account(): 
+    username = input("Enter your desired username: ")
+    password = getpass.getpass("Enter your desired password: ")
+    hashed_password = hashlib.sha256(password.encode()).hexdigest() 
+    password_manager[username] = hashed_password 
+    print("Account created successfully!")
+
+def login(): 
+    username = input("Enter your username: ") 
+    password = getpass.getpass("Enter your password: ") 
+    hashed_password = hashlib.sha256(password.encode()).hexdigest() 
+    if username in password_manager.keys() and password_manager[username] == hashed_password: 
+        print("Login successful!")
+    else: 
+        print("Invalid username or password.")
+
