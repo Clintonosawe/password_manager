1f9db1d117733f65dc0db28b9b433859ef3674a0
Clinton Osawe <osaweclinton1@gmail.com>
2025-09-20 22:44:05 -0400

    added create_account function

--- password_manager.py
+++ password_manager.py
@@ -3,4 +3,9 @@ import getpass #Module helps to hide string
 
 password_manager = {} 
 
-
+def create_account(): 
+    username = input("Enter your desired username: ")
+    password = getpass.getpass("Enter your desired password: ")
+    hashed_password = hashlib.sha256(password.encode()).hexdigest() 
+    password_manager[username] = hashed_password 
+    print("Account created successfully!")
